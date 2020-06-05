44bd3c4e8e375538ca68f64464ada5c88c1d3ed1
Karla Gardiner <karla@macbook-pro.home>
2020-06-05 20:35:47 +0100

    Implemented a sort 0s and 1s algorithm

--- lib/sort_0s_and_1s.rb
+++ lib/sort_0s_and_1s.rb
@@ -1,1 +1,18 @@
 # Given an array containing only 0s and 1s, sort it.
+class Array
+
+  def my_sort_01s
+    sorted_array = []
+
+    self.each do |element|
+      if element === 0
+        sorted_array.unshift(element)
+      else
+        sorted_array.push(element)
+      end
+    end
+
+    return sorted_array
+  end
+
+end
